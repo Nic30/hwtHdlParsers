d65cc482a7674b1fcab35ed11cdde86b7ab9ae6c
nic30 <nic30@seznam.cz>
2017-06-12 00:01:19 +0200

    fix import

--- hwtHdlParsers/hierarchyExtractor.py
+++ hwtHdlParsers/hierarchyExtractor.py
@@ -1,16 +1,18 @@
 from copy import copy
 import hdlConvertor
 
+from hwt.pyUtils.arrayQuery import arr_any
 from hwtHdlParsers.defaults import defaultIgnoredRefs
 from hwtHdlParsers.referenceCollector import collectReferences
-from python_toolkit.arrayQuery import arr_any
 
 
 class UnresolvedReferenceError(Exception):
     pass
 
+
 class FileBoundedDict(dict):
     pass
+
 
 def includeRefToDict(d, ref, obj):
     nLastIndx = len(ref.names) - 1
@@ -24,7 +26,7 @@ def includeRefToDict(d, ref, obj):
         d = _d
         if i == nLastIndx:
             _d.designFile = obj
-             
+
 
 class DesignFile():
     """
@@ -33,13 +35,13 @@ class DesignFile():
     def __init__(self, fileInfo, jCtx):
         """
         @param fileInfo: ParserFileInfo objects contains filename, langue etc
-        @param jCtx: JSON serialized HDL AST (output from hdlConvertor.parse) 
+        @param jCtx: JSON serialized HDL AST (output from hdlConvertor.parse)
         """
         self.fileInfo = fileInfo
         self.required, self.declared, self.defined = collectReferences(jCtx, scope=[fileInfo.lib])
-        self.unresolvedDep = copy(self.required) 
-        
-        self.depOnDefinitions = set()  # this file depends on definitions in DesignFiles 
+        self.unresolvedDep = copy(self.required)
+
+        self.depOnDefinitions = set()  # this file depends on definitions in DesignFiles
         self.depOnDeclarations = set()  # declaration in this file has definition in DesignFiles
 
     @staticmethod
@@ -67,19 +69,19 @@ class DesignFile():
     def buildDefDecDict(allDesignFiles):
         topDef = {}
         topDec = {}
-        
+
         for df in allDesignFiles:
             for d in df.declared:
                 if hasattr(d, "defined") and d.defined:
                     includeRefToDict(topDef, d, df)
                 includeRefToDict(topDec, d, df)
-        
+
         for df in allDesignFiles:
             for d in df.defined:
                 includeRefToDict(topDef, d, df)
-                
+
         return topDec, topDef
-    
+
     @staticmethod
     def findInGlobals(globalDict, ref, scope=[]):
         """
@@ -96,10 +98,9 @@ class DesignFile():
             names = ref.names
             for n in names:
                 top = top[n]
-                
+
         return top
-            
-    
+
     def discoverDependentOnFiles(self, globalDecDict, globalDefDict, ignoredRefs=[]):
         """
         Discover on which files is this file dependent
@@ -108,58 +109,57 @@ class DesignFile():
         for d in self.required:
             if arr_any(ignoredRefs, lambda x: DesignFile.refMatch(d, x)):
                 continue
-            
+
             try:
                 fi = DesignFile.findInGlobals(imported, d, scope=[self.fileInfo.lib])
             except KeyError:
                 fi = None
-            
+
             if d.all:
                 _imported = DesignFile.findInGlobals(globalDecDict, d)
-                imported.update(_imported) 
+                imported.update(_imported)
 
             if fi is None:
                 try:
                     fi = DesignFile.findInGlobals(globalDecDict, d, scope=[self.fileInfo.lib])
                 except KeyError:
-                    raise UnresolvedReferenceError("%s: require to import %s" % 
-                                            (self.fileInfo.fileName, str(d)))
+                    raise UnresolvedReferenceError("%s: require to import %s" %
+                                                   (self.fileInfo.fileName, str(d)))
                 if fi is not self:
                     if isinstance(fi, FileBoundedDict):
                         fi = fi.designFile
-                    
+
                     self.depOnDeclarations.add(fi)
-        
+
         for d in self.declared:
             try:
                 fi = DesignFile.findInGlobals(globalDefDict, d, scope=[self.fileInfo.lib])
             except KeyError:
-                raise UnresolvedReferenceError("%s: can not find definition of %s" % 
-                                       (self.fileInfo.fileName, str(d)))
-                
+                raise UnresolvedReferenceError("%s: can not find definition of %s" %
+                                               (self.fileInfo.fileName, str(d)))
+
             if fi is not self:
                 if isinstance(fi, FileBoundedDict):
                     try:
                         fi = fi.designFile
                     except AttributeError:
-                        raise  UnresolvedReferenceError("%s: can not find definition of %s" % 
-                                       (self.fileInfo.fileName, str(d)))
+                        raise UnresolvedReferenceError("%s: can not find definition of %s" %
+                                                       (self.fileInfo.fileName, str(d)))
                 self.depOnDefinitions.add(fi)
-            
-        
+
     @staticmethod
     def loadFiles(fileInfos, debug=False):
         """
-        load ParserFileInfo and build DesignFile for it 
+        load ParserFileInfo and build DesignFile for it
         """
         designFiles = []
         for fi in fileInfos:
             jCtx = hdlConvertor.parse(fi.fileName, fi.lang,
-                                       hierarchyOnly=True,
-                                       debug=debug)
+                                      hierarchyOnly=True,
+                                      debug=debug)
             d = DesignFile(fi, jCtx)
             designFiles.append(d)
-        
+
         return designFiles
 
     @staticmethod
@@ -170,5 +170,3 @@ class DesignFile():
         globalDecDict, globalDefDict = DesignFile.buildDefDecDict(designFiles)
         for df in designFiles:
             df.discoverDependentOnFiles(globalDecDict, globalDefDict, ignoredRefs)
-
-
