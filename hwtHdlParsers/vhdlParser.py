6dcc816eea5a9d86c2af0dd51d57c84c125df1f9
nic30 <nic30@seznam.cz>
2017-06-24 11:47:57 +0200

    migrate parseVhdlBitstring from hwt

--- hwtHdlParsers/vhdlParser.py
+++ hwtHdlParsers/vhdlParser.py
@@ -3,6 +3,31 @@ from hwt.synthesizer.rtlLevel.mainBases import RtlSignalBase
 from hwtHdlParsers.baseParser import BaseParser
 
 
+def pushBit(v, b):
+    return (v << 1) | b
+
+
+def parseVhdlBitString(val, toType):
+    v = val.clone()
+    _v = v.val
+    v.val = 0
+    v.vldMask = 0
+    v._dtype = toType
+    for ch in reversed(_v):
+        if ch == '1':
+            v.val = pushBit(v.val, 1)
+            v.vldMask = pushBit(v.vldMask, 1)
+        elif ch == '0':
+            v.val = pushBit(v.val, 0)
+            v.vldMask = pushBit(v.vldMask, 1)
+        elif ch == 'x':
+            v.val = pushBit(v.val, 0)
+            v.vldMask = pushBit(v.vldMask, 0)
+        else:
+            raise TypeError("found %s in bitstring literal" % (ch))
+    return v
+
+
 class VhdlParser(BaseParser):
 
     def opFromJson(self, jOp, ctx):
@@ -11,7 +36,7 @@ class VhdlParser(BaseParser):
         ops = [op0]
         if operator == AllOps.TERNARY or operator == AllOps.CALL:
             for jOperand in jOp['operands']:
-                operand = self.exprFromJson(jOperand, ctx) 
+                operand = self.exprFromJson(jOperand, ctx)
                 ops.append(operand)
             if operator == AllOps.CALL and isinstance(ops[0], RtlSignalBase):
                 operator = AllOps.INDEX
@@ -21,5 +46,5 @@ class VhdlParser(BaseParser):
                 assert l['type'] == "ID"
                 ops.append(l['value'])
             else:
-                ops.append(self.exprFromJson(jOp['op1'], ctx)) 
+                ops.append(self.exprFromJson(jOp['op1'], ctx))
         return operator._evalFn(*ops)
